bdef2098e6274a0e20158fe40f44b930998667ea
johnnieskywalker <lwolski0@gmail.com>
2023-02-22 00:27:44 +0100

    wip uniswap playground

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { getAmountOut } from './uniswap';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('getAmountOut', () => {
+  it('should return the correct amount of COMP for a given amount of USDC', async () => {
+    const usdcAmount = 1000; // 1000 USDC
+    const expectedAmount = 24.627; // Expected amount of COMP based on current exchange rate
+
+    const amountOut = await getAmountOut(usdcAmount);
+    console.log('amountOut',amountOut);
+    expect(amountOut).toBeCloseTo(expectedAmount, 3);
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getAmountOut, getPath } from "./uniswap";
+
+
+const path = getPath();
+
 
 function App() {
+  console.log('json rpc node')
+console.log(process.env.REACT_APP_JSON_RPC_NODE_URL);
+
+  const [usdcAmount, setUsdcAmount] = useState(0);
+  const [compAmount, setCompAmount] = useState(0);
+  
+  const handleUsdcAmountChange = async (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const amount = parseFloat(event.target.value);
+    setUsdcAmount(amount);
+    const amountOut = await getAmountOut(amount);
+    console.log('amountOut',amountOut)
+    setCompAmount(amountOut);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>
+        USDC amount:
+        <input
+          type="number"
+          value={usdcAmount}
+          onChange={handleUsdcAmountChange}
+        />
+      </label>
+      <br />
+      <label>
+        COMP amount:
+        <input type="number" value={compAmount} disabled />
+      </label>
+      <br />
+      <p>Swap path: {path.join(" -> ")}</p>
+      {process.env.REACT_APP_JSON_RPC_NODE_URL}
+      {process.env.REACT_APP_DUPA}
+      {`${process.env}`}
     </div>
   );
 }
