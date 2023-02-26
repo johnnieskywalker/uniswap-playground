79fa28524304edc7225c28a77929933aad7e13a5
johnnieskywalker <lwolski0@gmail.com>
2023-02-26 01:28:20 +0100

    small polishing

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,28 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { getAmountOut, getPath } from "./uniswap";
 
 function App() {
-
   const SUGGESTED_INITIAL_USDC_AMOUNT = 2000;
   const [usdcAmount, setUsdcAmount] = useState(SUGGESTED_INITIAL_USDC_AMOUNT);
   const [compAmount, setCompAmount] = useState("0");
-  const [path, setPath] = useState("Loading the Path...");
-  
+  const [path, setPath] = useState("Specify USDC amount to find the path...");
+
   const handleUsdcAmountChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
     const amount = parseFloat(event.target.value);
     setUsdcAmount(amount);
     const amountOut = await getAmountOut(amount);
-    console.log('amountOut',amountOut)
+    console.log("amountOut", amountOut);
     setCompAmount(amountOut);
-    const optimalPath = await  getPath();
+    const optimalPath = await getPath();
     setPath(optimalPath);
   };
 
   return (
     <div>
-      <h1>Uniswap V2</h1>
+      <h1>Uniswap Universal Router USDC-COMP</h1>
       Change USDC amount to see the amount of COMP you will get.
       <br />
       <label>
@@ -38,7 +36,7 @@ function App() {
       <br />
       <label>
         COMP amount:
-        <input type="number" value={compAmount} disabled/>
+        <input type="number" value={compAmount} disabled />
       </label>
       <br />
       <p>Swap path: {path}</p>
--- src/uniswap.ts
+++ src/uniswap.ts
@@ -11,6 +11,14 @@ const COMP = new Token(ChainId.MAINNET, COMPOUND_ADDRESS, 18);
 const DEFAULT_HARDHAT_ADDRESS = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
 const provider = new JsonRpcProvider(process.env.REACT_APP_JSON_RPC_NODE_URL);
+const router = new AlphaRouter({ chainId: 1, provider });
+const options = {
+  recipient: DEFAULT_HARDHAT_ADDRESS,
+  slippageTolerance: new Percent(1, 100),
+  deadline: Math.floor(Date.now() / 1000 + 1800),
+  type: SwapType.UNIVERSAL_ROUTER,
+};
+let swapPathOutput = "Fetching path data...";
 
 async function getAmountOut(usdcAmount: number): Promise<string> {
   console.log("usdcAmount", usdcAmount);
@@ -18,17 +26,9 @@ async function getAmountOut(usdcAmount: number): Promise<string> {
     usdcAmount * Math.pow(10, USDC.decimals)
   ).toString();
   console.log("usdcInputConverted", usdcInputConverted);
-  const router = new AlphaRouter({ chainId: 1, provider });
   console.log("router", router);
-  const options = {
-    recipient: DEFAULT_HARDHAT_ADDRESS,
-    slippageTolerance: new Percent(1, 100),
-    deadline: Math.floor(Date.now() / 1000 + 1800),
-    type: SwapType.UNIVERSAL_ROUTER,
-  };
-  console.log("options", options);
 
-  const route = await getUsdcCompUniversalRoute();
+  const route = await getUsdcCompUniversalRoute(usdcInputConverted);
 
   if (route) {
     console.log("route", route);
@@ -42,24 +42,28 @@ async function getAmountOut(usdcAmount: number): Promise<string> {
     );
     console.log(`Gas Used USD: ${route.estimatedGasUsedUSD.toFixed(2)}`);
     console.log(route?.methodParameters?.calldata);
+    console.log("the path");
+    console.log(route.route[0].tokenPath);
+    swapPathOutput = route.route[0].tokenPath
+      .map((token) => `${token.address}`)
+      .join(" -> ");
   }
 
   return route
     ? route?.quote.toFixed(route.quote.currency.decimals)
     : "loading";
-
-  async function getUsdcCompUniversalRoute() {
-    return await router.route(
-      CurrencyAmount.fromRawAmount(USDC, usdcInputConverted),
-      COMP,
-      TradeType.EXACT_INPUT,
-      options
-    );
-  }
+}
+async function getUsdcCompUniversalRoute(usdcInputConverted: string) {
+  return await router.route(
+    CurrencyAmount.fromRawAmount(USDC, usdcInputConverted),
+    COMP,
+    TradeType.EXACT_INPUT,
+    options
+  );
 }
 
 async function getPath(): Promise<string> {
-  return "USDC -> COMP";
+  return swapPathOutput;
 }
 
 export { getAmountOut, getPath };
